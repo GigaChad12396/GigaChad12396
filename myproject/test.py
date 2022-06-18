ce87e875dc3e03a8be348cd70b7bfc8b1af3ae0e
gigachad <siddharthkon@gmail.com>
2022-06-18 19:46:44 +0530

    feature employee implemented

--- myproject/test.py
+++ myproject/test.py
@@ -1,1 +1,10 @@
-print("hello")
+from dataclasses import dataclass
+import uuid
+@dataclass
+class Employee:
+    name:str
+    age:int
+    employee_id:str
+siddhart = Employee(name="Siddarth",age=30,employee_id=uuid.uuid4())
+
+print(siddhart.age)
